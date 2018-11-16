35a5f9b43d47ee3d067c320f88796b66bb0edc57
codeomen <damioncms@gmail.com>
2018-11-15 21:35:28 -0600

    Add basic looping function pattern for each stream of characters

--- matrix.js
+++ matrix.js
@@ -1,4 +1,4 @@
-var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
 var randCharArray = []
   
@@ -6,4 +6,23 @@ for (let i=0; i < 30;i++) {
   randCharArray.push(chars.charAt(Math.random() * 30))
 }
 
-console.log(randCharArray)
+// console.log(randCharArray)
+
+// basic function looping through the Array of characters based on a random starting index
+function digiStream(newChars) {
+  //starting character index
+  let startChar = Math.round(Math.random() * 30)
+  // start loop
+  for (let i=startChar;i < 30;i++) {
+    console.log(newChars[i])
+    if (i==29) {
+      for (let i=0;i < startChar;i++) {
+        console.log(newChars[i])
+      }
+    }
+  }
+}
+
+
+digiStream(randCharArray)
+
