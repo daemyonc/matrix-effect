ad72adce1d4886c069555891ae6730d3dd73dfc7
Damion C <damioncms@gmail.com>
2018-12-18 23:00:55 -0600

    Add characters to DOM with basic styling

--- matrix.js
+++ matrix.js
@@ -1,23 +1,34 @@
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-
 var randCharArray = []
-  
-for (let i=0; i < 30;i++) {
-  randCharArray.push(chars.charAt(Math.random() * 30))
-}
 
+// generate random character array
+function genRandCharArray() {
+  for (let i=0; i < 30;i++) {
+    randCharArray.push(chars.charAt(Math.random() * 30))
+  }
+  return randCharArray
+}  
+
+genRandCharArray()
 // console.log(randCharArray)
 
 // basic function looping through the Array of characters based on a random starting index
-function digiStream(newChars) {
+function digiStream(charList) {
   //starting character index
   let startChar = Math.round(Math.random() * 30)
+  function addCharToDOM(char) {
+    let container = document.querySelector('.container')
+    let newDigit = document.createTextNode(char)
+    container.appendChild(newDigit)
+  }
   // start loop
   for (let i=startChar;i < 30;i++) {
-    console.log(newChars[i])
+    console.log(charList[i])
+    addCharToDOM(charList[i])
     if (i==29) {
       for (let i=0;i < startChar;i++) {
-        console.log(newChars[i])
+        console.log(charList[i])
+        addCharToDOM(charList[i])
       }
     }
   }
