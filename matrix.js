72e05713140956c351d6620bb2df973d98ea7337
Daemyon <damioncms@gmail.com>
2019-03-31 20:38:31 -0500

    Add column divs based on window width for inserting the cascading text

--- matrix.js
+++ matrix.js
@@ -1,4 +1,7 @@
 const chars = 'AZﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍ123456789'
+const emCount = window.innerWidth /
+  parseFloat(getComputedStyle(document.querySelector('body'))['font-size']);
+const columnCount = Math.round(emCount / 2);
 var randCharArray = []
 
 // generate random character array
@@ -7,26 +10,29 @@ function genRandCharArray(len) {
     randCharArray.push(chars.charAt(Math.random() * 36))
   }
   return randCharArray
+}
+genRandCharArray(32);
+
+function addCharToDOM(char,contName) {
+  let container = document.querySelector(contName)
+  let column = document.createElement('div')
+  let newDigit = document.createTextNode(char)
+  
+  column.appendChild(newDigit)
+  container.insertBefore(column,container.childNodes[0])
 }  
 
 // basic function looping through the Array of characters
 function digiStream(charList) {
 
-  function addCharToDOM(char) {
-    let container = document.querySelector('.container')
-    let column = document.createElement('div')
-    let newDigit = document.createTextNode(char)
-    
-    column.appendChild(newDigit)
-    container.insertBefore(column,container.childNodes[0])
-  }
+  
 
   // index variable for our timed loop
   var i = 0
   // start loop
   var timedLoop = setInterval( function() {
-    // console.log(charList[i])
-    addCharToDOM(charList[i])
+    
+    addCharToDOM(charList[i],'.container')
     
     if (i > 1) {
       let bow = Math.round((Math.random() * 10));
@@ -42,8 +48,26 @@ function digiStream(charList) {
     } 
   }, 50)
 }
+function insertColumns() {
+  // Generate columns based on width
 
-genRandCharArray(32)
+  for(let c=0;c < emCount;c++) {
+    let elem = document.querySelector('.container');
+    let newColDiv = document.createElement('div');
+    console.log(newColDiv);
+    elem.appendChild(newColDiv).classList.add('col'+c);
+  }
+}
 
-digiStream(randCharArray)
+/* Future possible code
+let i;
+setInterval(() => {
+  let randColumn = Math.round((Math.random() * emCount));
+  
+  
+},50) */
+
+insertColumns();
+
+digiStream(randCharArray);
 
